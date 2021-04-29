728bf7b1813fb86f935f06957c0c63db63a8be24
kosidocs <kelvinonaru@gmail.com>
2021-04-29 08:54:55 +0400

    add progress bar 6 components

---
+++ src/components/progressbar6/choice.js
@@ -0,0 +1,13 @@
+const questions = [
+    {
+        "question": "What is your level of education?",
+        "answers": [
+            { "answer": "High school or less", "icon": "1F4C3" },
+            { "answer": "Collage", "icon": "1F4D2" },
+            { "answer": "Diploma", "icon": "1F4DA" },
+            { "answer": "Batchelors", "icon": "1F4D5" }
+        ]
+    },
+];
+
+export default questions;
