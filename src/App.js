fe6a6dd44e5b48995a25547a246f5e3aa349c175
kosidocs <kelvinonaru@gmail.com>
2021-04-29 08:47:06 +0400

    feat: add router link for each pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/applyform/Wrapper'
+import Progress1 from "./components/progressbar1/Progress1"
+import Progress2 from "./components/progressbar2/Progressbar2"
+import Progress3 from "./components/progressbar3/Progressbar3"
+import Progress4 from "./components/progressbar4/Progressbar4"
+import Progress5 from "./components/progressbar5/Progressbar5"
+import Progress6 from "./components/progressbar6/Progressbar6"
+import Progress7 from "./components/progressbar7/Progressbar7"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/step/1">
+          <Progress1 />
+        </Route>
+        <Route exact path="/step/2">
+          <Progress2 />
+        </Route>
+        <Route exact path="/step/3">
+          <Progress3 />
+        </Route>
+        <Route exact path="/step/4">
+          <Progress4 />
+        </Route>
+        <Route exact path="/step/5">
+          <Progress5 />
+        </Route>
+        <Route exact path="/step/6">
+          <Progress6 />
+        </Route>
+        <Route exact path="/step/7">
+          <Progress7 />
+        </Route>
+        <Route exact path="/">
+          <Wrapper />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
