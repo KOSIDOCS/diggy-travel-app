7f11de4fc37ff985646a5c6318d1fb5e67c80d36
kosidocs <kelvinonaru@gmail.com>
2021-04-29 08:52:39 +0400

    feat: add progress bar 2 component

---
+++ src/components/progressbar2/choice.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        "question": "Why do you want to come to dubai?",
+        "answers": [
+          { "answer": "Lifestyle", "icon": "1F929" },
+          { "answer": "Work opportunities", "icon": "1F9D1-200D-1F4BC" },
+          { "answer": "Education", "icon": "1F4D4" },
+          { "answer": "Finance", "icon": "1F4B0" },
+          { "answer": "Tourism", "icon": "1F6EB" },
+          { "answer": "Family", "icon": "1F46A" },
+          { "answer": "Business", "icon": "1F454" },
+          { "answer": "Security", "icon": "E247" },
+          { "answer": "The weather", "icon": "1F326" },
+          { "answer": "Meeting new people", "icon": "1F91D" },
+          { "answer": "Culture", "icon": "1F3F5" }
+        ]
+      },
+]
+
+export default questions;
