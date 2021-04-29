f13cd4b055f02ca5c2f177ff827fb016815e34d8
kosidocs <kelvinonaru@gmail.com>
2021-04-29 08:55:24 +0400

    feat: add progress bar 7 components

---
+++ src/components/progressbar7/choice.js
@@ -0,0 +1,15 @@
+const questions = [
+    {
+        "question": "How soon do you want to come to Dubai?",
+        "answers": [
+            { "answer": "Right now", "icon": "1F4C5" },
+            { "answer": "0 to 3 months", "icon": "1F4C5" },
+            { "answer": "3 to 6 months", "icon": "1F4C5" },
+            { "answer": "6 to 12 months", "icon": "1F4C5" },
+            { "answer": "After 1 year", "icon": "1F4C5" },
+            { "answer": "I'm not sure", "icon": "1F4C5" }
+        ]
+    }
+];
+
+export default questions;
