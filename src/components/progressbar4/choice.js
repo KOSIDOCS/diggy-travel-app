6178c4175b5945a2aac533f528870415d9618c2d
kosidocs <kelvinonaru@gmail.com>
2021-04-29 08:53:59 +0400

    feat: add progress bar 4 components

---
+++ src/components/progressbar4/choice.js
@@ -0,0 +1,40 @@
+const questions = [
+    {
+        "question": "What are your interests?",
+        "answers": [
+            { "answer": "Outdoors", "icon": "1F3D5" },
+            { "answer": "Fitness", "icon": "1F3C3-200D-2642-FE0F" },
+            { "answer": "Health", "icon": "1F468-200D-2695-FE0F" },
+            { "answer": "Travel", "icon": "1F3D6" },
+            { "answer": "Theme parks", "icon": "1F334" },
+            { "answer": "Cooking", "icon": "1F37D" },
+            { "answer": "Sports", "icon": "1F3C9" },
+            { "answer": "Adventure", "icon": "1F3DE" },
+            { "answer": "Music", "icon": "1F3B5" },
+            { "answer": "Fashion", "icon": "1F380" },
+            { "answer": "Technology", "icon": "1F468-200D-1F4BB" },
+            { "answer": "Architecture", "icon": "1F3DF" },
+            { "answer": "Writing", "icon": "270D" },
+            { "answer": "Gaming", "icon": "1F3AE" },
+            { "answer": "Dining", "icon": "1F374" },
+            { "answer": "History", "icon": "1F5FF" },
+            { "answer": "Theatre", "icon": "1F3AD" },
+            { "answer": "Networking", "icon": "E1D0" },
+            { "answer": "Art", "icon": "1F9D1-200D-1F3A8" },
+            { "answer": "Politics", "icon": "1F935-200D-2642-FE0F" },
+            { "answer": "Fishing", "icon": "1F3A3" },
+            { "answer": "Meet people", "icon": "1F91D" },
+            { "answer": "Podcasts", "icon": "1F50A" },
+            { "answer": "Gambling", "icon": "1FA99" },
+            { "answer": "Self improvement", "icon": "1F44D" },
+            { "answer": "Foreign languages", "icon": "1F5E3" },
+            { "answer": "Movies", "icon": "1F3A6" },
+            { "answer": "Celebrities", "icon": "1F380 (1)" },
+            { "answer": "Dating", "icon": "1F48F" },
+            { "answer": "Social media", "icon": "E042" },
+            { "answer": "Photography", "icon": "1F4F7" }
+        ]
+    },
+];
+
+export default questions;
