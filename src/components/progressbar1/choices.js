7feeef60ddb2d10d8e9a05383cb6bd546d2b6539
kosidocs <kelvinonaru@gmail.com>
2021-04-29 08:52:04 +0400

    feat: add progress bar 1 components and view

---
+++ src/components/progressbar1/choices.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+        "question": "Have you ever visited Dubai?",
+        "answers": [
+            { "answer": "No", "icon": "1F44D (2)" },
+            { "answer": "Yes", "icon": "1F44D" }
+        ]
+    },
+    {
+        "question": "Do you have any friends or family in Dubai?",
+        "answers": [
+            { "answer": "No", "icon": "1F44D (2)" },
+            { "answer": "Yes", "icon": "1F44D" }
+        ]
+    },
+    {
+        "question": "How long do you want to stay in stay in Dubai?",
+        "answers": [
+            { "answer": "Upto 3 months", "icon": "1F5D3" },
+            { "answer": "3 to 6 months", "icon": "1F5D3" },
+            { "answer": "6 to 12 months", "icon": "1F5D3" },
+            { "answer": "+12 months", "icon": "1F5D3" },
+            { "answer": "+3 years", "icon": "1F5D3" }
+        ]
+    },
+];
+
+export default questions;
